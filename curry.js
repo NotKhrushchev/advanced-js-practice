507d73dda1f91cb21c0c29b7480c355406529a5a
Nikita Ipatov <nikita@Nikitas-MacBook-Air.local>
2025-05-20 18:02:25 +0300

    solved BFE.dev #2 - implement basic debounce

--- curry.js
+++ curry.js
@@ -7,12 +7,3 @@ function curry(fn) {
     }
   };
 }
-
-/** test cases */
-const join = (a, b, c) => {
-  return `${a}_${b}_${c}`;
-};
-const curriedJoin = curry(join);
-console.log(curriedJoin(1, 2, 3)); // '1_2_3'
-console.log(curriedJoin(1)(2, 3)); // '1_2_3'
-console.log(curriedJoin(1, 2)(3)); // '1_2_3'
---
+++ debounce.js
@@ -0,0 +1,7 @@
+function debounce(func, wait) {
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => func(...args), wait);
+  };
+}
